bd08a96abb7feee822da466f99dec3de0b90adcd
mo1ein <moeinn.com@gmail.com>
2020-10-09 02:00:22 +0330

    submit 66478449 for 122A(Lucky Division) with lang:GNU C++17 and verdict:OK

---
+++ 100-199/122/122A(Lucky Division).cpp
@@ -0,0 +1,2 @@
+#include <bits/stdc++.h>
+int main (){int n ;std::cin >> n;(n%4==0||n%7==0|n%47==0|n%74==0|n%44==0|n%77==0|n%477==0|n%747==0|n%774==0|n%777==0|n%444==0)? std::cout<<"YES": std::cout << "NO";}
